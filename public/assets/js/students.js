c4f62b15bc866ea8428c06a93eb3d9158fa82979
Kaden Almaraz <kalmaraz@brooksschool.org>
2026-05-15 11:00:38 -0400

    gpa and form table elements added

--- public/assets/js/students.js
+++ public/assets/js/students.js
@@ -4,9 +4,19 @@ fetch('/api/students').then(res => res.json()).then(students => buildTable(stude
 
 function buildTable(students) {
     console.log(students)
+
     for (let student of students) {
-        line = document.createElement('tl')
-        line.innerHTML = JSON.stringify(student)
-        doc.insertAdjacentElement('beforeend', line)
+        let row = document.createElement('tr')
+        doc.insertAdjacentElement('beforeend', row) 
+
+        addLine(student.form, row)
+        addLine(student.gpa, row)     
     }
+}
+
+function addLine(stud, row) {
+    line = document.createElement('tl')
+    line.innerHTML = stud // JSON.stringify(student)
+    row.insertAdjacentElement('beforeend', line) // https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
+    console.log('insert')
 }
