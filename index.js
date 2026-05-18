55c4c43a172c6c0e51910236e2c8f3520341af77
Kaden Almaraz <kalmaraz@brooksschool.org>
2026-05-18 14:07:37 -0400

    session login redirection

--- index.js
+++ index.js
@@ -11,26 +11,29 @@ const PORT = process.env.SERVER_PORT || 3000
 
 app.set('view engine', 'ejs');
 
-app.use(session({
-    secret: 'open session',
-    cookie: {maxAge: 30000},
-    saveUninitialized: false
-}))
+app.use(
+    session({
+        secret: process.env.SECRET_KEY,
+        resave: false,
+        saveUninitialized: false
+    })
+)
+
 app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 // -------
 // PAGES
 // -------
 
 app.get('/', (req, res) => {
-    res.render('index', {})
+    if (checkAuth(req, res)) res.render('index', {})
 })
 app.get('/login', (req, res) => {
     res.render('login', {})
 })
 app.get('/students', (req, res) => {
-    res.render('students', {})
+    if (checkAuth(req, res)) res.render('students', {})
 })
 
 // -------------
@@ -46,7 +49,7 @@ app.get('/api/students', (req, res) => {
 // -------
 
 app.listen(PORT, () => {
-    console.log(`Server Running on http://localhost:${PORT}/login`)
+    console.log(`Server Running on http://localhost:${PORT}`)
 })
 
 //--------
@@ -57,3 +60,16 @@ app.post('/authenticate-user', (req, res) => {
     let { email, password } = req.body
     let status = db.authenticate(email, password, res)
 }) 
+
+//-----------
+// FUNCTIONS
+//-----------
+
+function checkAuth(req, res) {
+    if (!req.session || !req.session.email) {
+        res.redirect('/login')
+        return false
+    }
+
+    return true
+}
