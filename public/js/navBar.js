a82ae375ade3e298e23a3c815528ac25efc2d5a1
Craig Gorton <cgorton@brooksschool.org>
2026-05-26 11:06:45 -0400

    navbar work

---
+++ public/js/navBar.js
@@ -0,0 +1,18 @@
+let navBar = document.getElementById('navBar')
+
+fetch('/api/classes').then(res => res.json()).then(classes => buildNav(classes))
+
+function buildNav(classes) {
+    console.log(classes)
+
+    for (let c of classes) {
+        // let row = document.createElement('tr')
+        // doc.insertAdjacentElement('beforeend', row)
+
+        // addLine(student.fname, row)
+        // addLine(student.lname, row)
+        // addLine(student.form, row)
+        // addLine(student.gpa, row)
+        // addLine(student['boarding/day'], row)
+    }
+}
