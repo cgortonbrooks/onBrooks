5d45ba1db504f02cd2697de583ffb8c3be70b516
Kevin Xie <kxie@brooksschool.org>
2026-05-26 00:59:58 -0400

    students finish

--- public/js/students.js
+++ public/js/students.js
@@ -1,25 +1,24 @@
 let doc = document.getElementById('studentTable')
 
-fetch('/api/students').then(res => res.json()).then(students => buildTable(students)) // https://www.youtube.com/watch?v=2DVbW0Szqh0
+fetch('/api/students').then(res => res.json()).then(students => buildTable(students))
 
 function buildTable(students) {
     console.log(students)
 
     for (let student of students) {
         let row = document.createElement('tr')
-        doc.insertAdjacentElement('beforeend', row) 
+        doc.insertAdjacentElement('beforeend', row)
 
         addLine(student.fname, row)
         addLine(student.lname, row)
         addLine(student.form, row)
-        addLine(student.gpa, row)  
-        addLine(student['boarding/day'], row)   
+        addLine(student.gpa, row)
+        addLine(student['boarding/day'], row)
     }
 }
 
 function addLine(stud, row) {
-    line = document.createElement('td')
-    line.innerHTML = stud // JSON.stringify(student)
-    row.insertAdjacentElement('beforeend', line) // https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
-    console.log('insert', line)
+    let line = document.createElement('td')
+    line.innerHTML = stud
+    row.insertAdjacentElement('beforeend', line)
 }
