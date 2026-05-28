e868b6ba8f905ca0990eeb5c703d996577a4cf80
James Skowronek <jskowronek@brooksschool.org>
2026-05-28 12:57:15 -0400

    Class page dynamically updated with database info

--- public/js/class.js
+++ public/js/class.js
@@ -1,5 +1,13 @@
 let page = document.getElementById('content')
+const params = new URLSearchParams(window.location.search);
+const classId = params.get('class');
 
-fetch('/api/class').then(res => res.json()).then(i => console.log(i[0].name))
+fetch(`/api/class?class=${classId}`).then(res => res.json()).then(i => buildPage(i))
 
-//function buildPage(info)
+function buildPage(info) {
+    let header = document.getElementById('title')
+    let general = document.getElementById('info')
+
+    header.innerHTML = info[0].name
+    general.innerHTML = `${info[0].teacher_id} | Class ID ${info[0].classID} | Room ${info[0].room} | Block ${info[0].section}` 
+}
